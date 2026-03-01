177a351329a391db0e7da35d7042643a53a77269
github-action-benchmark <github@users.noreply.github.com>
2026-03-01 22:46:51 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 71c16412f59713e75f2d30302be00bd59e42d675

---
+++ dev/bench/data.js
@@ -0,0 +1,86 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1772405211678,
+  "repoUrl": "https://github.com/Wyydra/rx",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "paulchopinet@gmail.com",
+            "name": "Wyydra",
+            "username": "Wyydra"
+          },
+          "committer": {
+            "email": "paulchopinet@gmail.com",
+            "name": "Wyydra",
+            "username": "Wyydra"
+          },
+          "distinct": true,
+          "id": "71c16412f59713e75f2d30302be00bd59e42d675",
+          "message": "ci",
+          "timestamp": "2026-03-01T23:45:29+01:00",
+          "tree_id": "f00fdac46502357bab200faa11c6dc054bd23470",
+          "url": "https://github.com/Wyydra/rx/commit/71c16412f59713e75f2d30302be00bd59e42d675"
+        },
+        "date": 1772405210747,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ackermann/lua",
+            "value": 17.2569,
+            "unit": "ms",
+            "extra": "mean=17.2493ms σ=0.3154ms n=50"
+          },
+          {
+            "name": "ackermann/py",
+            "value": 129.887,
+            "unit": "ms",
+            "extra": "mean=129.9558ms σ=2.5847ms n=50"
+          },
+          {
+            "name": "ackermann/rxt",
+            "value": 24.4605,
+            "unit": "ms",
+            "extra": "mean=24.5271ms σ=0.2815ms n=50"
+          },
+          {
+            "name": "fib/lua",
+            "value": 54.552,
+            "unit": "ms",
+            "extra": "mean=54.6527ms σ=0.5371ms n=50"
+          },
+          {
+            "name": "fib/py",
+            "value": 117.0844,
+            "unit": "ms",
+            "extra": "mean=117.853ms σ=1.6785ms n=50"
+          },
+          {
+            "name": "fib/rxt",
+            "value": 86.5711,
+            "unit": "ms",
+            "extra": "mean=87.2755ms σ=3.524ms n=50"
+          },
+          {
+            "name": "greet/lua",
+            "value": 1.1186,
+            "unit": "ms",
+            "extra": "mean=1.1359ms σ=0.0466ms n=50"
+          },
+          {
+            "name": "greet/py",
+            "value": 11.4037,
+            "unit": "ms",
+            "extra": "mean=11.425ms σ=0.1081ms n=50"
+          },
+          {
+            "name": "greet/rxt",
+            "value": 0.4908,
+            "unit": "ms",
+            "extra": "mean=0.5117ms σ=0.0645ms n=50"
+          }
+        ]
+      }
+    ]
+  }
+}
