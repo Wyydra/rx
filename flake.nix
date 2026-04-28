a00826db8e7d0f388850fbf41a7c4b24e7ce0e5d
Wyydra <paulchopinet@gmail.com>
2026-04-28 15:54:41 +0200

    update zig version

--- flake.nix
+++ flake.nix
@@ -8,29 +8,26 @@
       inputs.systems.follows = "systems";
     };
     zig-overlay.url = "github:mitchellh/zig-overlay";
-    zls = {
-      url = "github:zigtools/zls";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.zig-overlay.follows = "zig-overlay";
-    };
   };
 
   outputs =
-    { nixpkgs, flake-utils, zig-overlay, zls, ... }:
+    { nixpkgs, flake-utils, zig-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        zig = zig-overlay.packages.${system}."0.16.0";
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ zig-overlay.overlays.default ];
+          overlays = [ 
+            (final: prev: {
+                inherit zig;
+             })
+          ];
         };
-        zig = pkgs.zigpkgs.master;
-        zls_pkg = zls.packages.${system}.zls;
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ pkg-config wasmtime python3 just lua valgrind kdePackages.kcachegrind gtk4 xorg.libXcursor]
-                     ++ [ zig zls_pkg ];
+          packages = with pkgs; [ zig zls pkg-config wasmtime python3 just lua valgrind kdePackages.kcachegrind gtk4 xorg.libXcursor];
           shellHook = ''
             export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath (with pkgs; [
               wayland
